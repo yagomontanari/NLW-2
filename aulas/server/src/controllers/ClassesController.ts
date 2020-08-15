858d3651b9536da8ace3413e5e6e3ce634431891
yMontanari <yagomontanari2@gmail.com>
2020-08-15 18:20:59 -0300

    Terceiro dia

--- aulas/server/src/controllers/ClassesController.ts
+++ aulas/server/src/controllers/ClassesController.ts
@@ -37,7 +37,7 @@ export default class ClassesController {
                     .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
                     .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
                     .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
-                    .whereRaw('`class_schedule`.`to` >= ??', [timeInMinutes])
+                    .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes])
             })
 
 
--- aulas/web/src/components/TeacherItem/index.tsx
+++ aulas/web/src/components/TeacherItem/index.tsx
@@ -3,33 +3,53 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css'
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FunctionComponent<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://pbs.twimg.com/profile_images/1252817847429894152/Kh4GFovk_400x400.jpg" alt="Yago Montanari" />
+                <img src={teacher.avatar} alt="" />
                 <div>
-                    <strong>Yago Montanari</strong>
-                    <span>ReactJS</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Professor Extraordinário de ReactJS
-            <br /><br />
-            Especialista em layouts, desenvolvimento de software com JavaScript, React, ReactJS, ReactNative, DOM, etc...
-        </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora
-              <strong>R$ 80,00</strong>
+                <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a
+                    target="blank"
+                    onClick={createNewConnection}
+                    href={`https://wa.me/${teacher.whatsapp}`}>
                     <img src={whatsappIcon} alt="Whatsapp" />
                 Entrar em contato
-            </button>
+          </a>
             </footer>
         </article>
     );
